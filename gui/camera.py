f3e0ee2ec7d2a36539cf0c8d61f2105519927b24
neel216 <neelr216@gmail.com>
2020-10-30 12:58:13 -0400

    added camera compatability for raspberry pi

--- gui/camera.py
+++ gui/camera.py
@@ -2,6 +2,14 @@ import tkinter as tk
 from tkinter import ttk
 import cv2
 from PIL import Image, ImageTk
+try:
+    from picamera.array import PiRGBArray
+    from picamera import PiCamera
+    import time
+    rpi = True
+except ImportError:
+    print('Not on Raspberry Pi')
+    rpi = False
 
 class Camera:
     def __init__(self, parent, menu, mainMenu, nextPage):
@@ -20,9 +28,15 @@ class Camera:
         self.lmain = tk.Label(self.camera)
         self.lmain.grid(row=1, column=0)
 
-        self.cap = cv2.VideoCapture(0)
-        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 464)
-        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 464)
+        if rpi:
+            self.camera = PiCamera()
+            self.camera.resolution = (464, 464)
+            self.camera.framerate = 32
+            self.rawCapture = PiRGBArray(self.camera, size=self.camera.resolution)
+        else:
+            self.cap = cv2.VideoCapture(0)
+            self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 464)
+            self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 464)
 
         takePicture = ttk.Button(self.camera, text='Take Picture', command=self.mainMenu.capture_image)
         takePicture.grid(row=2, column=0)
@@ -33,15 +47,26 @@ class Camera:
         self.video_stream()
 
     def video_stream(self):
-        _, self.frame = self.cap.read()
-        #self.frame = cv2.flip(frame, 1)
-        cv2image = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGBA)
-        img = Image.fromarray(cv2image)
-        img.thumbnail((464, 464), Image.ANTIALIAS)
-        imgtk = ImageTk.PhotoImage(image=img)
-        self.lmain.imgtk = imgtk
-        self.lmain.configure(image=imgtk)
-        self.lmain.after(10, self.video_stream)
+        if rpi:
+            for frame in self.camera.capture_continuous(self.rawCapture, format='bgr', use_video_port=True):
+                image = frame.array
+                cv2image = cv2.cvtColor(image, cv2.COLOR_BGR2RGBA)
+                img = Image.fromarray(cv2image)
+                img.thumbnail((464, 464), Image.ANTIALIAS)
+                imgtk = ImageTk.PhotoImage(image=img)
+                self.lmain.imgtk = imgtk
+                self.lmain.configure(image=imgtk)
+                self.lmain.after(10, self.video_stream)
+        else:
+            _, self.frame = self.cap.read()
+            #self.frame = cv2.flip(frame, 1)
+            cv2image = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGBA)
+            img = Image.fromarray(cv2image)
+            img.thumbnail((464, 464), Image.ANTIALIAS)
+            imgtk = ImageTk.PhotoImage(image=img)
+            self.lmain.imgtk = imgtk
+            self.lmain.configure(image=imgtk)
+            self.lmain.after(10, self.video_stream)
 
     def get_picture(self):
         #self.cap.release()
