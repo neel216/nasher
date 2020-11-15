23a9bf78912bf69c6ac2495312a872128bf7a7f5
neel216 <neelr216@gmail.com>
2020-11-15 17:16:30 -0500

    change for video demo

--- gui/mainMenu.py
+++ gui/mainMenu.py
@@ -153,7 +153,7 @@ class MainMenu:
         print('Capturing Image')
         cam = camera_ocr.CamOCR()
         self.objectID = cam.run()
-        self.objectID = '1994.8.1'
+        self.objectID = '1987.9.6'
         self.selection = selection.Selection(self.parent, self.mainMenu, self, self.lookup, self.width, location=self.location, objectID=self.objectID)
         self.verification = verification.Verification(self.parent, self.mainMenu, self, self.entry, self.selection, self.objectID)
         self.verification.show()
